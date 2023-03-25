cde3bc0f5259a9e4c767a7b47e21376367b16f05
Matthias Schmid <111773387+MatthiasSchmid93@users.noreply.github.com>
2023-03-25 22:28:13 +0100

    Update main.py

--- main.py
+++ main.py
@@ -10,10 +10,10 @@ import os
 from mutagen.id3 import ID3, APIC
 
 
-class _file:
-    rate = 0
-    _type = ""
-    filename = ""
+class File:
+    rate = None
+    type = None
+    filename = None
     tags = {
         "Title": "TIT2",
         "BPM": "TBPM",
@@ -24,204 +24,194 @@ class _file:
         "Track number": "TRCK",
         "Genre": "TCON",
     }
-
-
-def open_audio(file):
-    _file.rate = 0
-    _file._type = ""
-    _file.filename = ""
+    
+    
+def check_file(file):
+    try:
+        with open(file) as _: pass
+    except (FileNotFoundError, PermissionError): return False
     if matches := re.search(r"(.+)\.(.+)$", file):
-        _file.filename = matches.group(1)
-        try:
-            if matches.group(2) == "wav":
-                audio = AudioSegment.from_wav(file)
-                _file.rate = audio.frame_rate
-                _file._type = matches.group(2)
-            elif matches.group(2) == "mp3":
-                audio = AudioSegment.from_mp3(file)
-                _file.rate = audio.frame_rate
-                _file._type = matches.group(2)
-            else:
-                print("File not mp3 or wav")
-                return 0
-        except FileNotFoundError:
-            print("File not found")
-            return 0
-    else:
-        print("Unsupported filetype")
-        return 0
-
-    # return audio signal as 2D numpy array
+        if matches.group(2) == "mp3" or matches.group(2) == "wav":
+            File.filename = matches.group(1)
+            File.type = matches.group(2)
+            return True
+    else: return False
+    
+    
+def open_audio(file):
+    if File.type == "mp3":
+        audio = AudioSegment.from_mp3(file)
+        File.rate = audio.frame_rate
+    elif File.type == "wav":
+        audio = AudioSegment.from_wav(file)
+        File.rate = audio.frame_rate
     return np.array(audio.get_array_of_samples(), dtype=np.int16).reshape(-1, 2)
 
 
-def extract_audio(audio_arr):
+def remove_backround_noise(signal_arr):
+    signal_arr[signal_arr == 1] = 0
+    signal_arr[signal_arr == -1] = 0
 
-    # Replace all -1s and 1s with zero to avoid backround noise
-    audio_arr[audio_arr == 1] = 0
-    audio_arr[audio_arr == -1] = 0
+    
+def split_audio_signal(signal_arr):
+    
+    signal_left_org, signal_right_org = np.split(signal_arr, 2, axis=1)
 
-    # Split original signal
-    audio_left_org, audio_right_org = np.split(audio_arr, 2, axis=1)
+    left_positive = np.array(signal_left_org, dtype=np.int32)
+    left_negative = np.array(signal_left_org, dtype=np.int32)
+    right_positive = np.array(signal_right_org, dtype=np.int32)
+    right_negative = np.array(signal_right_org, dtype=np.int32)
 
-    left_positive = np.array(audio_left_org, dtype=np.int32)
-    left_negative = np.array(audio_left_org, dtype=np.int32)
-    right_positive = np.array(audio_right_org, dtype=np.int32)
-    right_negative = np.array(audio_right_org, dtype=np.int32)
-
+    # replace positve ints with 1 and vice versa
+    # in negative arr 0 is also replaced for later merging 
     left_positive[left_positive < 0] = 1
     left_negative[left_negative >= 0] = -1
     right_positive[right_positive < 0] = 1
     right_negative[right_negative >= 0] = -1
+    
+    # Turn negative ints to postive for simplicity
+    left_negative = abs(left_negative)
+    right_negative = abs(right_negative)
 
-    splited_signal = [
+    split_signal = [
         left_positive,
-        abs(left_negative),
+        left_negative,
         right_positive,
-        abs(right_negative),
+        right_negative,
     ]
-    splited_signal_copy = [
+    split_signal_copy = [
         np.array(left_positive, dtype=np.int32),
-        np.array(abs(left_negative), dtype=np.int32),
+        np.array(left_negative, dtype=np.int32),
         np.array(right_positive, dtype=np.int32),
-        np.array(abs(right_negative), dtype=np.int32),
+        np.array(right_negative, dtype=np.int32),
     ]
 
-    return splited_signal, splited_signal_copy
+    return split_signal, split_signal_copy
 
 
-def find_peak_limit(audio):
-    duration = len(audio) / _file.rate / 60
+def find_transient_limit(signal):
+    duration = len(signal) / File.rate / 60
     if duration < 0.03:
         return 1
-    number_of_peaks = (6 / 5) * duration
-    peak_limit = number_of_peaks * 1.6 * (60 * duration)
+    number_of_transients = (6 / 5) * duration
+    transient_limit = number_of_transients * 1.6 * (60 * duration)
 
-    return peak_limit
+    return transient_limit
 
 
-def find_peaks(splited_signal, peak_limit):
-    peaks = []
+def find_transients(split_signal, transient_limit):
+    transients = []
     max_volume = 32767
-    with alive_bar(len(splited_signal), title="Searching Peaks") as bar:
-        for i, signal in enumerate(splited_signal):
+    with alive_bar(len(split_signal), title="Searching Peaks") as bar:
+        for signal in split_signal:
             np.extract(signal > max_volume, signal)
             while True:
-                found_peaks = np.extract(signal >= max_volume, signal).size
+                found_transients = np.extract(signal >= max_volume, signal).size
                 max_volume -= 100
-                if found_peaks > peak_limit:
-                    peaks.append(np.where(signal >= max_volume)[0])
+                if found_transients > transient_limit:
+                    transients.append(np.where(signal >= max_volume)[0])
                     max_volume = 32767
                     break
             bar()
-    return peaks
+    return transients
 
 
-def find_transients(splited_signal, peaks):
-    transients = [[], [], [], []]
+def find_amplitudes(split_signal, transients):
+    amplitudes = [[], [], [], []]
 
-    def find_transient(x, i, splited_signal):
+    def find_amplitude(x, i, split_signal):
         _i = 0
         while True:
-            if splited_signal[x][peaks[x][i] - _i] <= 1:
-                start = peaks[x][i] - _i
+            if split_signal[x][transients[x][i] - _i] <= 1:
+                start = transients[x][i] - _i
                 _i = 0
                 break
             _i += 1
         while True:
-            if splited_signal[x][peaks[x][i] + _i] <= 1:
-                end = peaks[x][i] + _i
+            if split_signal[x][transients[x][i] + _i] <= 1:
+                end = transients[x][i] + _i
                 break
             _i += 1
-        transients[x].append([start, end])
+        amplitudes[x].append([start, end])
 
     for x in range(4):
-        for i in range(len(peaks[x])):
+        for i in range(len(transients[x])):
             if i > 1:
                 if (
-                    transients[x][len(transients[x]) - 1][0]
-                    <= peaks[x][i]
-                    <= transients[x][len(transients[x]) - 1][1]
+                    amplitudes[x][len(amplitudes[x]) - 1][0]
+                    <= transients[x][i]
+                    <= amplitudes[x][len(amplitudes[x]) - 1][1]
                 ):
                     pass
                 else:
-                    find_transient(x, i, splited_signal)
+                    find_amplitude(x, i, split_signal)
             else:
-                find_transient(x, i, splited_signal)
+                find_amplitude(x, i, split_signal)
 
-        if transients[x][0][1] >= transients[x][1][0]:
-            del transients[x][0]
+        if amplitudes[x][0][1] >= amplitudes[x][1][0]:
+            del amplitudes[x][0]
 
-    return transients
-
-
-def find_global_peak(splited_signal_copy, transients, x):
-    for i in range(len(transients[x])):
-        splited_signal_copy[x][transients[x][i][0] : transients[x][i][1]] = 0
-    return 32767 / splited_signal_copy[x].max()
+    return amplitudes
 
 
-def normalise(splited_signal, splited_signal_copy, transients):
-    def _normalise(splited_signal, start, end, factor):
-        splited_signal[start:end][splited_signal[start:end] > 2] = (
-            splited_signal[start:end][splited_signal[start:end] > 2] * factor
+def amplify(split_signal, split_signal_copy, amplitudes):
+    
+    def find_global_peak(split_signal_copy, amplitudes, x):
+        for i in range(len(amplitudes[x])):
+            split_signal_copy[x][amplitudes[x][i][0] : amplitudes[x][i][1]] = 0
+        return 32767 / split_signal_copy[x].max()
+    
+    def _amplify(split_signal, start, end, factor):
+        split_signal[start:end][split_signal[start:end] > 2] = (
+            split_signal[start:end][split_signal[start:end] > 2] * factor
         )
 
     for x in range(4):
-        global_peak = find_global_peak(splited_signal_copy, transients, x)
-        first_tran = transients[x][0][0]
-        _normalise(splited_signal[x], 0, first_tran, global_peak)
-        with alive_bar(len(transients[x]), title="Normalising") as bar:
-            for i in range(len(transients[x])):
-                start_tran = transients[x][i][0]
-                end_tran = transients[x][i][1]
-                last_tran = transients[x][len(transients[x]) - 1][1]
-                end_audio = len(splited_signal[x])
+        global_peak = find_global_peak(split_signal_copy, amplitudes, x)
+        first_tran = amplitudes[x][0][0]
+        _amplify(split_signal[x], 0, first_tran, global_peak)
+        with alive_bar(len(amplitudes[x]), title="Normalising") as bar:
+            for i in range(len(amplitudes[x])):
+                start_tran = amplitudes[x][i][0]
+                end_tran = amplitudes[x][i][1]
+                last_tran = amplitudes[x][len(amplitudes[x]) - 1][1]
+                end_audio = len(split_signal[x])
                 try:
-                    end_between = transients[x][i + 1][0]
+                    end_between = amplitudes[x][i + 1][0]
                 except IndexError:
                     pass
-                peak_area_values = splited_signal[x][start_tran:end_tran]
-                normalised_peak_area = peak_area_values * global_peak
-
+                peak_area_values = split_signal[x][start_tran:end_tran]
+                normalised_peak_area = split_signal[x][start_tran:end_tran] * global_peak
                 if normalised_peak_area.max() > 32767:
                     local_peak = 32767 / peak_area_values.max()
-                    _normalise(splited_signal[x], start_tran, end_tran, local_peak)
+                    _amplify(split_signal[x], start_tran, end_tran, local_peak)
                 else:
-                    _normalise(splited_signal[x], start_tran, end_tran, global_peak)
-                _normalise(splited_signal[x], end_tran, end_between, global_peak)
+                    _amplify(split_signal[x], start_tran, end_tran, global_peak)
+                _amplify(split_signal[x], end_tran, end_between, global_peak)
                 bar()
         # Insert normalised block from last peak until end
-        _normalise(splited_signal[x], last_tran, end_audio, global_peak)
+        _amplify(split_signal[x], last_tran, end_audio, global_peak)
 
 
-def unsplit_signal(splited_signal):
+def check_for_cliping(split_signal):
     for i in range(4):
-        splited_signal[i][splited_signal[i] > 32767] = 32767
+        split_signal[i][split_signal[i] > 32767] = 32767
+        
 
-    splited_signal[1] = np.negative(splited_signal[1])
-    splited_signal[3] = np.negative(splited_signal[3])
+def merge_split_signal(split_signal):
+    
+    split_signal[1] = np.negative(split_signal[1])
+    split_signal[3] = np.negative(split_signal[3])
 
-    audio_left = (
-        np.array([splited_signal[0], splited_signal[1]], dtype=np.int16)
-        .transpose()
-        .flatten()
-    )
-    audio_right = (
-        np.array([splited_signal[2], splited_signal[3]], dtype=np.int16)
-        .transpose()
-        .flatten()
-    )
+    signal_left = (np.array([split_signal[0], split_signal[1]], dtype=np.int16).transpose().flatten())
+    signal_right = (np.array([split_signal[2], split_signal[3]], dtype=np.int16).transpose().flatten())
 
-    audio_left = np.delete(audio_left, np.where(audio_left == -1))
-    audio_right = np.delete(audio_right, np.where(audio_right == -1))
-    audio_left = np.delete(audio_left, np.where(audio_left == 1))
-    audio_right = np.delete(audio_right, np.where(audio_right == 1))
+    signal_left = np.delete(signal_left, np.where(signal_left == -1))
+    signal_right = np.delete(signal_right, np.where(signal_right == -1))
+    signal_left = np.delete(signal_left, np.where(signal_left == 1))
+    signal_right = np.delete(signal_right, np.where(signal_right == 1))
 
-    audio_left = np.array(audio_left, dtype=np.int16)
-    audio_right = np.array(audio_right, dtype=np.int16)
-
-    return np.column_stack((audio_left, audio_right))
+    return np.column_stack((signal_left, signal_right))
 
 
 def write_tags(file):
@@ -230,67 +220,49 @@ def write_tags(file):
     try:
         # Album art
         pict = tags_old.getall("APIC")[0].data
-        tags_new.add(
-            APIC(encoding=3, mime="image/jpg", type=3, desc="Cover", data=pict)
-        )
+        tags_new.add(APIC(encoding=3, mime="image/jpg", type=3, desc="Cover", data=pict))
     except:
         print("Album Cover not found")
-    for tag in _file.tags:
+    for tag in File.tags:
         try:
-            tags_new[_file.tags[tag]] = tags_old[_file.tags[tag]]
+            tags_new[File.tags[tag]] = tags_old[File.tags[tag]]
         except:
             print(f"{tag} tag not found")
     tags_new.save(f"./Normalised Files/{file}", v2_version=3)
 
 
-def save(splited_signal):
-    if _file._type == "mp3":
-        song = AudioSegment(
-            unsplit_signal(splited_signal).tobytes(),
-            frame_rate=_file.rate,
-            sample_width=2,
-            channels=2,
-        )
-        song.export(
-            f"./Normalised Files/{_file.filename}.{_file._type}",
-            format="mp3",
-            bitrate="320k",
-        )
-    elif _file._type == "wav":
-        write(
-            f"./Normalised Files/{_file.filename}.{_file._type}",
-            _file.rate,
-            unsplit_signal(splited_signal),
-        )
+def save(split_signal):
+    if File.type == "mp3":
+        song = AudioSegment(split_signal.tobytes(), frame_rate=File.rate, sample_width=2, channels=2,)
+        song.export(f"./Normalised Files/{File.filename}.{File.type}", format="mp3", bitrate="320k",)
+    elif File.type == "wav":
+        write(f"./Normalised Files/{File.filename}.{File.type}", File.rate, split_signal)
 
 
-def normalise_audio(file):
+def normalise(file):
     try:
-        audio_arr = open_audio(file)
-        if type(audio_arr) == np.ndarray:
-            splited_signal, splited_signal_copy = extract_audio(audio_arr)
-            peaks = find_peaks(splited_signal, find_peak_limit(splited_signal[0]))
-            normalise(
-                splited_signal,
-                splited_signal_copy,
-                find_transients(splited_signal, peaks),
-            )
-            save(splited_signal)
-            write_tags(f"{_file.filename}.{_file._type}")
-            print("\033[92m SUCCESS \033[0m")
-    except:
-        print("\033[91m FAILED \033[0m")
+        signal_arr = open_audio(file)
+        remove_backround_noise(signal_arr)
+        split_signal, split_signal_copy = split_audio_signal(signal_arr)
+        transients = find_transients(split_signal, find_transient_limit(split_signal[0])) 
+        amplify(split_signal, split_signal_copy, find_amplitudes(split_signal, transients))
+        check_for_cliping(split_signal)
+        signal_arr = merge_split_signal(split_signal)
+        save(signal_arr)
+        write_tags(f"{File.filename}.{File.type}")
+        print("\033[92m SUCCESS \033[0m")
+    except: print("\033[91m FAILED \033[0m")
 
 
 def main():
     try:
         os.makedirs("Normalised Files")
-    except FileExistsError:
-        pass
-    done_files = os.listdir("./Normalised Files")
+    except FileExistsError: pass
+    doneFiles = os.listdir("./Normalised Files")
     for file in os.listdir("./"):
-        if file not in done_files:
-            normalise_audio(file)
+        if file not in doneFiles:
+            if check_file(file) == True:
+                normalise(file)
 
 
 if __name__ == "__main__":
