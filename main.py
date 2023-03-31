8cc661fd6fe9baa54799c1b49a11dba9732e3989
Matthias Schmid <111773387+MatthiasSchmid93@users.noreply.github.com>
2023-03-31 18:23:38 +0200

    Update main.py

--- main.py
+++ main.py
@@ -55,34 +55,35 @@ class ArrayModifiers:
     def combine_arrays(array_pair: list) -> np.ndarray:
         return np.column_stack((array_pair[0], array_pair[1]))
 
-    def split_signal_array(signal_array) -> list:
-        signal_left_org, signal_right_org = np.split(signal_array, 2, axis=1)
-        split_signals = [
-            np.array(signal_left_org, dtype=np.float32),
-            np.array(signal_left_org, dtype=np.float32),
-            np.array(signal_right_org, dtype=np.float32),
-            np.array(signal_right_org, dtype=np.float32),
+    def split_array(array) -> list:
+        split_array1, split_array2 = np.split(array, 2, axis=1)
+        split_arrays = [
+            np.array(split_array1, dtype=np.float32),
+            np.array(split_array1, dtype=np.float32),
+            np.array(split_array2, dtype=np.float32),
+            np.array(split_array2, dtype=np.float32),
         ]
-        return split_signals
+        return split_arrays
 
-    def merge_split_signal(split_signal: list) -> list:
+    def merge_split_arrays(split_arrays: list) -> list:
         signal_left = np.array(
-            [split_signal[0], split_signal[1]], dtype=np.int16).T.flatten()
+            [split_arrays[0], split_arrays[1]], dtype=np.int16).T.flatten()
         signal_right = np.array(
-            [split_signal[2], split_signal[3]], dtype=np.int16).T.flatten()
+            [split_arrays[2], split_arrays[3]], dtype=np.int16).T.flatten()
         return [signal_left, signal_right]
 
 
 class Normalizer:
 
-    def find_transient_limit(signal_array: np.ndarray, frame_rate: int) -> int:
+    # todo # make search threshold smarter
+    def find_search_treshold(signal_array: np.ndarray, frame_rate: int) -> int:
         duration = len(signal_array) / frame_rate / 60
         if duration < 0.03:
             return 1
-        number_of_transients = (6 / 10) * duration
+        number_of_transients = (6 / 15) * duration
         return int(number_of_transients * 1.6 * (60 * duration))
 
-    def find_transients(signal_array: np.ndarray, transient_limit: int) -> np.ndarray:
+    def find_transients(signal_array: np.ndarray, search_treshold: int) -> np.ndarray:
         threshold = 16383
         step = 8192
         reached_above, reached_below = 0, 0
@@ -90,20 +91,21 @@ class Normalizer:
         while True:
             found_transients = np.extract(
                 signal_array >= threshold, signal_array).size
-            if found_transients > transient_limit:
+            if found_transients > search_treshold:
                 if reached_below:
                     step /= 2
                 threshold += step
                 reached_above, reached_below = 1, 0
-            elif found_transients < transient_limit:
+            elif found_transients < search_treshold:
                 if reached_above:
                     step /= 2
                 threshold -= step
                 reached_below, reached_above = 1, 0
-            else:
-                return np.where(signal_array >= threshold)[0]
-            if step <= 1:
-                return np.where(signal_array >= threshold)[0]
+            if step <= 1 or found_transients == search_treshold:
+                if threshold < 5:
+                    return np.array([])
+                else:
+                    return np.where(signal_array >= threshold)[0]
 
     def find_amplitudes(signal_array: np.ndarray, transients: np.ndarray) -> list:
         amplitudes = []
@@ -181,7 +183,7 @@ class File:
                 "filename": f"{name}{ext}",
                 "type": ext,
                 "frame_rate": audio.frame_rate,
-                "signal_arr": signal_array}
+                "signal_array": signal_array}
             return file_data
         except (FileNotFoundError, PermissionError, IsADirectoryError):
             return None
@@ -215,60 +217,62 @@ class File:
 
         tags_new.save(f"./Normalised Files/{file}", v2_version=3)
 
-    def save(signal_array, file_name: str, file_type: str, frame_rate: int) -> None:
-        if file_type == ".mp3":
+    def save(signal_array: np.ndarray, audio_file_data: dict) -> None:
+        if audio_file_data["type"] == ".mp3":
             audio = AudioSegment(signal_array.tobytes(
-            ), frame_rate=frame_rate, sample_width=2, channels=2,)
+            ), frame_rate=audio_file_data["frame_rate"], sample_width=2, channels=2,)
             audio.export(
-                f"./Normalised Files/{file_name}", format="mp3", bitrate="320k",)
-        elif file_type == ".wav":
-            write(f"./Normalised Files/{file_name}", frame_rate, signal_array)
+                f"./Normalised Files/{audio_file_data['filename']}", format="mp3", bitrate="320k",)
+        elif audio_file_data["type"] == ".wav":
+            write(
+                f"./Normalised Files/{audio_file_data['filename']}", audio_file_data["frame_rate"], signal_array)
 
 
-def open_file_and_modulate_signal_arrays(file: str) -> list:
-    if audio_file_data := File.open_audio(file):
-        ArrayModifiers.replace_equals_with_values(
-            audio_file_data['signal_arr'], EQUAL_VALUES, REPLACE_VALUES)
-        split_signal_arrays = ArrayModifiers.split_signal_array(
-            audio_file_data['signal_arr'])
-        ArrayModifiers.replace_negatives_with_value(
-            split_signal_arrays, [SIGNAL_LEFT_POSITIVE, SIGNAL_RIGHT_POSITIVE], 1)
-        ArrayModifiers.replace_positives_with_value(
-            split_signal_arrays, [SIGNAL_LEFT_NEGATIVE, SIGNAL_RIGHT_NEGATIVE], -1)
-        ArrayModifiers.invert_values(
-            split_signal_arrays, [SIGNAL_LEFT_NEGATIVE, SIGNAL_RIGHT_NEGATIVE])
-        return split_signal_arrays, audio_file_data
-    return None
-
-
-def normalize(split_signal_arrays: list, audio_file_data: dict) -> list:
-    for signal_array_index in range(len(split_signal_arrays)):
-        signal_array = split_signal_arrays[signal_array_index]
-        transient_limit = Normalizer.find_transient_limit(
-            signal_array, audio_file_data['frame_rate'])
-        transients = Normalizer.find_transients(signal_array, transient_limit)
-        if type(transients) == np.ndarray:
-            ArrayModifiers.remove_adjacent_by_difference(transients)
-            amplitudes = Normalizer.find_amplitudes(signal_array, transients)
-            amplification_factor = Normalizer.find_amplification_factor(
-                signal_array, amplitudes)
-            Normalizer.amplify(signal_array, amplification_factor, amplitudes)
-            Normalizer.check_for_cliping(signal_array)
-            return split_signal_arrays, audio_file_data
-        else:
-            return None, None
-
-
-def modulate_signal_arrays_and_save(split_signal_arrays: list, audio_file_data: dict) -> None:
+def split_array_and_modify(array: np.ndarray) -> list:
+    ArrayModifiers.replace_equals_with_values(
+        array, EQUAL_VALUES, REPLACE_VALUES)
+    arrays = ArrayModifiers.split_array(array)
+    ArrayModifiers.replace_negatives_with_value(
+        arrays, [SIGNAL_LEFT_POSITIVE, SIGNAL_RIGHT_POSITIVE], 1)
+    ArrayModifiers.replace_positives_with_value(
+        arrays, [SIGNAL_LEFT_NEGATIVE, SIGNAL_RIGHT_NEGATIVE], -1)
     ArrayModifiers.invert_values(
-        split_signal_arrays, [SIGNAL_LEFT_NEGATIVE, SIGNAL_RIGHT_NEGATIVE])
-    signals_left_right = ArrayModifiers.merge_split_signal(split_signal_arrays)
+        arrays, [SIGNAL_LEFT_NEGATIVE, SIGNAL_RIGHT_NEGATIVE])
+    return arrays
+
+
+def normalize(signal_array: list, audio_file_data: dict) -> bool:
+    search_treshold = Normalizer.find_search_treshold(
+        signal_array, audio_file_data['frame_rate'])
+    transients = Normalizer.find_transients(signal_array, search_treshold)
+    if transients.any():
+        ArrayModifiers.remove_adjacent_by_difference(transients)
+        amplitudes = Normalizer.find_amplitudes(signal_array, transients)
+        amplification_factor = Normalizer.find_amplification_factor(
+            signal_array, amplitudes)
+        Normalizer.amplify(signal_array, amplification_factor, amplitudes)
+        Normalizer.check_for_cliping(signal_array)
+        return 1
+    else:
+        return 0
+
+
+def modify_and_merge_arrays(signal_arrays: list) -> np.ndarray:
+    ArrayModifiers.invert_values(
+        signal_arrays, [SIGNAL_LEFT_NEGATIVE, SIGNAL_RIGHT_NEGATIVE])
+    signals_left_right = ArrayModifiers.merge_split_arrays(signal_arrays)
     ArrayModifiers.delete_values(signals_left_right, DELETE_VALUES)
-    audio_signal = ArrayModifiers.combine_arrays(signals_left_right)
-    File.save(audio_signal, audio_file_data['filename'],
-              audio_file_data['type'], audio_file_data['frame_rate'])
-    File.write_tags(audio_file_data['filename'])
-    print("\033[92m SUCCESS \033[0m")
+    signal_array = ArrayModifiers.combine_arrays(signals_left_right)
+    return signal_array
+
+
+def run_multithreaded(function: callable, audio_file_data: dict, signal_arrays: list) -> bool:
+    with concurrent.futures.ThreadPoolExecutor(max_workers=4) as executor:
+        futures = [executor.submit(function, signal_array, audio_file_data)
+                   for signal_array in signal_arrays]
+        done, _ = concurrent.futures.wait(futures)
+        results = [future.result() for future in done]
+        return all(results)
 
 
 def main():
@@ -279,7 +283,18 @@ def main():
     done_files = os.listdir("./Normalised Files")
     for file in os.listdir("./"):
         if file not in done_files:
-            ...
+            if audio_file_data := File.open_audio(file):
+                print(file)
+                signal_arrays = split_array_and_modify(
+                    audio_file_data["signal_array"])
+                if not run_multithreaded(normalize, audio_file_data, signal_arrays):
+                    print("\033[96m SILENCE OR NO NORMALISATION NEEDED \033[0m")
+                    continue
+                signal_array = modify_and_merge_arrays(signal_arrays)
+                File.save(signal_array, audio_file_data)
+                File.write_tags(file)
+                print("\033[92m NORMALISED \033[0m")
+
 
 if __name__ == "__main__":
     main()
