9bcd246fcc87973adc576d1f0aa4b628e1744926
Matthias Schmid <111773387+MatthiasSchmid93@users.noreply.github.com>
2023-04-04 03:55:50 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-import concurrent.futures
 from mutagen.id3 import ID3, APIC
 import os
 from scipy.io.wavfile import write
@@ -17,20 +16,16 @@ SIGNAL_RIGHT_NEGATIVE = 3
 EQUAL_VALUES = [1, -1]
 REPLACE_VALUES = [0, 0]
 DELETE_VALUES = [1, -1]
+MAX_VOLUME = 32767
 
 
 class ArrayModifiers:
 
-    def remove_adjacent_by_difference(arrays: list, diff=1) -> None:
-        for arr_index in range(len(arrays)):
-            i = 0
-            while i < arrays[arr_index].size - 1:
-                if abs(arrays[arr_index][i] - arrays[arr_index][i+1]) == diff:
-                    arrays[arr_index] = np.delete(arrays[arr_index], i)
-                else:
-                    i += 1
+    def remove_adjacent_by_difference(array: np.ndarray, diff=1) -> np.ndarray:
+        diff_array = np.abs(np.diff(array)) == diff
+        return np.concatenate(([array[0]], array[1:][~diff_array]))
 
-    def replace_equals_with_values(array, equals: list, values: list) -> None:
+    def replace_equals_with_values(array: np.ndarray, equals: list, values: list) -> None:
         for equals, values in zip(equals, values):
             array[array == equals] = values
 
@@ -55,7 +50,7 @@ class ArrayModifiers:
     def combine_arrays(array_pair: list) -> np.ndarray:
         return np.column_stack((array_pair[0], array_pair[1]))
 
-    def split_array(array) -> list:
+    def split_array(array: np.ndarray) -> list:
         split_array1, split_array2 = np.split(array, 2, axis=1)
         split_arrays = [
             np.array(split_array1, dtype=np.float32),
@@ -71,68 +66,38 @@ class ArrayModifiers:
         signal_right = np.array(
             [split_arrays[2], split_arrays[3]], dtype=np.int16).T.flatten()
         return [signal_left, signal_right]
-
+    
 
 class Normalizer:
 
-    # todo # make search threshold smarter
-    def find_search_treshold(signal_array: np.ndarray, frame_rate: int) -> int:
-        duration = len(signal_array) / frame_rate / 60
-        if duration < 0.03:
-            return 1
-        number_of_transients = (6 / 15) * duration
-        return int(number_of_transients * 1.6 * (60 * duration))
-
-    def find_transients(signal_array: np.ndarray, search_treshold: int) -> np.ndarray:
-        threshold = 16383
-        step = 8192
-        reached_above, reached_below = 0, 0
-
-        while True:
-            found_transients = np.extract(
-                signal_array >= threshold, signal_array).size
-            if found_transients > search_treshold:
-                if reached_below:
-                    step /= 2
-                threshold += step
-                reached_above, reached_below = 1, 0
-            elif found_transients < search_treshold:
-                if reached_above:
-                    step /= 2
-                threshold -= step
-                reached_below, reached_above = 1, 0
-            if step <= 1 or found_transients == search_treshold:
-                if threshold < 5:
-                    return np.array([])
-                else:
-                    return np.where(signal_array >= threshold)[0]
+    def find_transient_threshold(signal_array: np.ndarray, frame_rate: int) -> list:
+        blocks_max = []
+        for i in range(0, signal_array.size, frame_rate):
+            block = signal_array[i:i+frame_rate]
+            blocks_max.append(int(block.max()))
+            threshold = int(sum(blocks_max) / len(blocks_max))
+        return threshold
+    
+    def find_transients(signal_array, threshold):
+        return np.where(signal_array >= threshold)[0]
 
     def find_amplitudes(signal_array: np.ndarray, transients: np.ndarray) -> list:
         amplitudes = []
+        indices_of_ones = np.where(signal_array == 1)[0]
         for transient in transients:
-            start, end = None, None
-            for trans in range(transient, -1, -1):
-                if signal_array[trans] <= 1:
-                    start = trans
-                    break
-            for trans in range(transient, len(signal_array)):
-                if signal_array[trans] <= 1:
-                    end = trans
-                    break
-            if amplitudes:
-                if amplitudes[-1][1] >= start:
-                    continue
-            amplitudes.append([start, end])
+            before_index = np.searchsorted(indices_of_ones, transient, side='left') - 1
+            after_index = np.searchsorted(indices_of_ones, transient, side='right')
+            amplitudes.append([indices_of_ones[before_index], indices_of_ones[after_index]])
         return amplitudes
 
     def find_amplification_factor(signal_array: np.ndarray, amplitudes: list) -> list:
         masked_signal_array = signal_array.copy()
-
+        
         for start, end in amplitudes:
             masked_signal_array[start: end] = 0
-
+            
         amplification_factor = np.float32(
-            32767 / np.max(masked_signal_array))
+            MAX_VOLUME / np.max(masked_signal_array))
         if amplification_factor < 1:
             amplification_factor = 1.0
         return amplification_factor
@@ -144,15 +109,15 @@ class Normalizer:
 
         def amplify_segment(start: int, end: int, factor: float) -> None:
             segment = signal_array[start:end]
-            above_threshold = segment > 2
+            above_threshold = segment > 3
             segment[above_threshold] *= factor
 
         amplify_segment(0, FIRST_AMP, amplification_factor)
 
         for i, (start, end) in enumerate(amplitudes):
             segment = signal_array[start:end] * amplification_factor
-            if segment.max() > 32767:
-                factor = 32767 / segment.max()
+            if segment.max() > MAX_VOLUME:
+                factor = MAX_VOLUME / (segment.max() / amplification_factor)
             else:
                 factor = amplification_factor
             amplify_segment(start, end, factor)
@@ -163,12 +128,12 @@ class Normalizer:
         amplify_segment(LAST_AMP, AUDIO_END, amplification_factor)
 
     def check_for_cliping(signal_array: np.ndarray) -> None:
-        signal_array[signal_array > 32767] = 32767
+        signal_array[signal_array > MAX_VOLUME] = MAX_VOLUME
 
 
 class File:
 
-    def open_audio(file) -> dict:
+    def open_audio(file: str) -> dict:
         name, ext = os.path.splitext(file)
         if ext.lower() not in {'.mp3', '.wav'}:
             return None
@@ -199,10 +164,12 @@ class File:
             "Track number": "TRCK",
             "Genre": "TCON",
         }
-        tags_old = ID3(file)
-        tags_new = ID3(f"./Normalised Files/{file}")
         try:
-            # Album art
+            tags_old = ID3(file)
+            tags_new = ID3(f"./Normalised Files/{file}")
+        except:
+            print("No ID3 Tags found")
+        try:
             pict = tags_old.getall("APIC")[0].data
             tags_new.add(APIC(encoding=3, mime="image/jpg",
                          type=3, desc="Cover", data=pict))
@@ -242,19 +209,15 @@ def split_array_and_modify(array: np.ndarray) -> list:
 
 
 def normalize(signal_array: list, audio_file_data: dict) -> bool:
-    search_treshold = Normalizer.find_search_treshold(
-        signal_array, audio_file_data['frame_rate'])
-    transients = Normalizer.find_transients(signal_array, search_treshold)
-    if transients.any():
-        ArrayModifiers.remove_adjacent_by_difference(transients)
-        amplitudes = Normalizer.find_amplitudes(signal_array, transients)
-        amplification_factor = Normalizer.find_amplification_factor(
-            signal_array, amplitudes)
-        Normalizer.amplify(signal_array, amplification_factor, amplitudes)
-        Normalizer.check_for_cliping(signal_array)
-        return 1
-    else:
-        return 0
+    threshold = Normalizer.find_transient_threshold(signal_array, audio_file_data["frame_rate"])
+    transients = Normalizer.find_transients(signal_array, threshold)
+    transients = ArrayModifiers.remove_adjacent_by_difference(transients)
+    amplitudes = Normalizer.find_amplitudes(signal_array, transients)
+    amplification_factor = Normalizer.find_amplification_factor(
+        signal_array, amplitudes)
+    Normalizer.amplify(signal_array, amplification_factor, amplitudes)
+    Normalizer.check_for_cliping(signal_array)
+    return 1
 
 
 def modify_and_merge_arrays(signal_arrays: list) -> np.ndarray:
@@ -264,15 +227,6 @@ def modify_and_merge_arrays(signal_arrays: list) -> np.ndarray:
     ArrayModifiers.delete_values(signals_left_right, DELETE_VALUES)
     signal_array = ArrayModifiers.combine_arrays(signals_left_right)
     return signal_array
-
-
-def run_multithreaded(function: callable, audio_file_data: dict, signal_arrays: list) -> bool:
-    with concurrent.futures.ThreadPoolExecutor(max_workers=4) as executor:
-        futures = [executor.submit(function, signal_array, audio_file_data)
-                   for signal_array in signal_arrays]
-        done, _ = concurrent.futures.wait(futures)
-        results = [future.result() for future in done]
-        return all(results)
 
 
 def main():
@@ -287,9 +241,10 @@ def main():
                 print(file)
                 signal_arrays = split_array_and_modify(
                     audio_file_data["signal_array"])
-                if not run_multithreaded(normalize, audio_file_data, signal_arrays):
-                    print("\033[96m SILENCE OR NO NORMALISATION NEEDED \033[0m")
-                    continue
+                for signal_array in signal_arrays:
+                    if not normalize(signal_array, audio_file_data):
+                        print("\033[96m SILENCE OR NO NORMALISATION NEEDED \033[0m")
+                        continue
                 signal_array = modify_and_merge_arrays(signal_arrays)
                 File.save(signal_array, audio_file_data)
                 File.write_tags(file)
