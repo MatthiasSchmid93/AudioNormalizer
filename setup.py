441393f328441d6ebe825085e5bce9c3dd495fc7
Matthias Schmid <schmid.mat93@gmail.com>
2023-10-26 00:30:43 +0200

    updated encoding and decoding for calling ffmpeg directly

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_namespace_packages
 
 setup(
     name='AudioNormalizer',
-    version='1.2.0',
+    version='1.3.0',
     packages=find_namespace_packages(),
     include_package_data=True,
     install_requires=[
